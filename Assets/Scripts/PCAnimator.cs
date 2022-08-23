e2741838e3e7b26c6e2de71d01d7590f3fcab9ba
DV <david_ventura_m@hotmail.com>
2022-08-23 15:15:11 -0400

    fixed animator c:

---
+++ Assets/Scripts/PCAnimator.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PCAnimator : VariableGrabber
+{
+    public Animator animator;
+
+    protected override void OnUpdate()
+    {
+        // This handles the jump animation
+        Vector3 tempSpeed = PlayerVelocity;
+        tempSpeed.y = 0;
+        animator.SetFloat("Speed", tempSpeed.magnitude);
+
+        //This handles the attacking animation
+        animator.SetBool("IsAttacking", IsAttacking);
+
+        //This handles the death animation
+        animator.SetBool("IsDead", IsDead);
+
+        //This handles the jumping animation
+        animator.SetBool("IsJumping", IsJumping);
+
+        //This handles the death animation
+        animator.SetBool("IsDead", IsDead);
+
+        //This is where the character is dead animation should go
+        print(IsJumping);
+    }
+
+
+
+}
