ea3a7eb9f266b7f7e1b0311447f3584fdd0564c5
Severencir <SeverencirDev@gmail.com>
2022-08-27 17:29:18 -0500

    player settings

--- Assets/Scripts/Attacks/Attack.cs
+++ Assets/Scripts/Attacks/Attack.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Threading;
-using UnityEditor.UIElements;
 using UnityEngine;
 
 public class Attack : MonoBehaviour
